9c25a02a5590729fe4ee7a787179c4dc50e74b63
Horleryheancarh <oladipoolayinka7@gmail.com>
2023-02-01 20:57:05 +0000

    Fix

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -114,7 +114,7 @@ class Rectangle:
         rectangle = ""
 
         if self.width == 0 or self.height == 0:
-            return 0
+            return rectangle
 
         for i in range(self.height):
             rectangle += ("#" * self.width) + "\n"
