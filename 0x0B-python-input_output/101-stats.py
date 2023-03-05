db207b0afd85bcf85e1e428c6b68414276086925
Horleryheancarh <oladipoolayinka7@gmail.com>
2023-03-05 17:21:26 +0000

    fixes

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -56,10 +56,9 @@ if __name__ == '__main__':
             if nlines % 10 == 0 and nlines != 0:
                 magic.print_info()
 
-            try:
-                list_line = [x for x in line.split(" ") if x.strip()]
-                magic.add_status_code(list_line[-2])
-                magic.size += int(list_line[-1].strip("\n"))
+            list_line = [x for x in line.split(" ") if x.strip()]
+            magic.add_status_code(list_line[-2])
+            magic.size += int(list_line[-1].strip("\n"))
 
             nlines += 1
 
