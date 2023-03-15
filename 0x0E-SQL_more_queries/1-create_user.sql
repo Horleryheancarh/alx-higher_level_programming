6990d9377f8d29978f948470673c66ff599d920c
Horleryheancarh <oladipoolayinka7@gmail.com>
2023-03-15 15:06:03 +0000

    test fixes

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,6 @@
 -- Create a new user
 -- Query to create a new user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+-- Set user password
+IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -2,5 +2,5 @@
 -- Query to create a table
 CREATE TABLE IF NOT EXISTS force_name (
 	id INT,
-	name VARCHAR256() NOT NULL
+	name VARCHAR(256) NOT NULL
 );
