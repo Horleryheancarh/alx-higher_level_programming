d784782f322a5f6bd9fd2da0201aa1a651e3482e
Horleryheancarh <oladipoolayinka7@gmail.com>
2023-03-16 08:00:09 +0000

    fixes

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,5 @@
 -- List shows and reviews
-SELECT tv_shows.title, SUM(tv_show_rating.rate) AS rating FROM tv_shows
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating FROM tv_shows
 	-- Get ratings
 	JOIN tv_show_ratings
 		ON tv_show_ratings.show_id = tv_shows.id
