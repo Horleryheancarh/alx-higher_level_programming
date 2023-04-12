0dde288f6f5e04d8df046c7981d38baab91b08d8
Horleryheancarh <oladipoolayinka7@gmail.com>
2023-04-12 23:40:00 +0000

    JavaScript beginner

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+let num = parseInt(process.argv[2]);
+if (Number.isNaN(num)) {
+  console.log('Missing number of occurrences');
+} else {
+  for ( num; num > 0; num--) {
+    console.log('C is fun');
+  }
+}
