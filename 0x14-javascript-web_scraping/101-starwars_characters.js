7424698e1da8617494fc31335436fd66bfbd9c77
Horleryheancarh <oladipoolayinka7@gmail.com>
2023-05-23 16:02:23 +0000

    fixes

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,13 +1,18 @@
 #!/usr/bin/node
 const request = require('request');
 const apiUrl = 'https://swapi-api.alx-tools.com/api/films/';
-const id = process.argv[2];
+let id = parseInt(process.argv[2]);
 let characters = [];
 
 request(apiUrl, function (err, res, body) {
   if (err == null) {
     const obj = JSON.parse(body);
     const results = obj.results;
+    if (id < 4) {
+      id += 3;
+    } else {
+      id -= 3;
+    }
     for (let i = 0; i < results.length; i++) {
       if (results[i].episode_id === id) {
         characters = results[i].characters;
@@ -21,5 +26,7 @@ request(apiUrl, function (err, res, body) {
         }
       });
     }
+  } else {
+    console.log(err);
   }
 });
